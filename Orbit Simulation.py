40b9c0c42fbab6b6166739fb5c68f2c99aa2127b
Wizard-of-Gauze <147918707+Wizard-of-Gauze@users.noreply.github.com>
2023-10-16 00:18:57 +0100

    Update Orbit Simulation.py

--- Orbit Simulation.py
+++ Orbit Simulation.py
@@ -7,7 +7,8 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Transit Simulation')
 
 AU = 1.495978707e11
-dt = 43200
+days = 0.5
+dt = 86400*days
 
 yellow = (255, 255, 0)
 dark_grey = (80, 78, 81)
@@ -89,21 +90,23 @@ class Planet:
                 self.velocity = self.velocity + velocity_update
                 self.location_orbit.append(self.location)
                 self.velocity_orbit.append(self.velocity)
+                if len(self.location_orbit) > 1400:
+                    del self.location_orbit[0]
             
 def main():
     run = True
     clock = pygame.time.Clock()
     
     sun = Planet('Sun', np.array([0, 0]), np.array([0, 0]), 1.98892 * 10**30, yellow, 30)
-    #asteroid = Planet('Asteroid', np.array([-0.5*AU, 0]), np.array([0, 5000]), 1 * 10**18, brown, 8)
+    #asteroid = Planet('Asteroid', np.array([-0.5*AU, 0]), np.array([0, 5000]), 1.98892 * 10**30, brown, 8)
     mercury = Planet('Mercury', np.array([-0.4*AU, 0]), np.array([0, 47360]), 3.285 * 10**23, dark_grey, 10)
     venus = Planet('Venus', np.array([-0.72*AU, 0]), np.array([0, 35020]), 4.867 * 10**24, white, 15)
     earth = Planet('Earth', np.array([-1*AU,0]), np.array([0, 29783]), 5.9742 * 10**24, dark_blue, 15)
     mars = Planet('Mars', np.array([-1.52*AU,0]), np.array([0, 24080]), 6.39 * 10**23, red, 12)
-    #jupiter = Planet('Jupiter', np.array([5.2*AU, 0]), np.array([0, 13060]), 1.89813 * 10**27, orange)
-    #saturn = Planet('Saturn', np.array([9.5*AU, 0]), np.array([0, 9670]), 5.683 * 10**26, gold)
-    #uranus = Planet('Uranus', np.array([19.8*AU, 0]), np.array([0, 6790]), 8.681 * 10**25, blue)
-    #neptune = Planet('Neptune', np.array([30.1*AU, 0]), np.array([0, 5450]), 1.024 * 10**26, dark_blue)
+    #jupiter = Planet('Jupiter', np.array([5.2*AU, 0]), np.array([0, 13060]), 1.89813 * 10**27, orange, 18)
+    #saturn = Planet('Saturn', np.array([9.5*AU, 0]), np.array([0, 9670]), 5.683 * 10**26, gold, 18)
+    #uranus = Planet('Uranus', np.array([19.8*AU, 0]), np.array([0, 6790]), 8.681 * 10**25, blue, 15)
+    #neptune = Planet('Neptune', np.array([30.1*AU, 0]), np.array([0, 5450]), 1.024 * 10**26, dark_blue, 15)
     
     planets = [sun, mercury, venus, earth, mars]
     
